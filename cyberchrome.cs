71c866d046556ffa47182ba68f829725e819bd16
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-06 16:35:48 +1000

    Add files via upload

---
+++ cyberchrome.cs
@@ -0,0 +1,22 @@
+string Filename = @"cyberchrome.cs";
+class cyberchrome;
+public class cyberchrome;
+{
+	//Cyberchrome type = Google Chrome
+	//Cyberchrome type = Browser
+	//Cyberchrome type = Google Chrome mod
+	//Cyberchrome type = Google Chrome script
+	//Cyberchrome type = Google Chrome random version
+	//Cyberchrome repaired performance
+	//Cyberchrome repaired framerate
+	//Cyberchrome repaired frame
+	//Cyberchrome repaired load
+	//Cyberchrome repaired RAM
+	//Cyberchrome repaired CPU
+	//Cyberchrome repaired GPU
+	//Cyberchrome repaired Extensions
+	//Cyberchrome repaired tabs
+	//Cyberchrome repaired Usage
+}
+
+
