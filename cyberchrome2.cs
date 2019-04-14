1d8b215b00db1c56ecaf9202cf161737ac32a2ec
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-14 18:23:28 +1000

    Add files via upload

---
+++ cyberchrome2.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Андрей
+ * Date: 14.04.2019
+ * Time: 18:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+//Cyberchrome type Google Chrome
+//Cyberchome type application
+namespace cyber();
+{
+	void cyberchrome();
+	void google_chrome();
+}
+
+//Cyberchrome type Browser
+//Cyberchrome type application
+//Cyberchrome random file directory = @"chrome.exe"
+namespace cyber2();
+{
+	void browser();
+	void cyberchrome();
+	void application();
+}
+
+//Cyberchrome optimized
+//Cybercrome fixed performance
+//Cyberchrome hight performance
+//Cyberchrome fixed waiting
+namespace cyber3();
+{
+	void cyberchrome();
+	void optimized();
+	void waiting();
+	void performance();
+	void google_chrome();
+}
+
+//Cyberchrome CPU repaired
+//Cyberchrome GPU repaired
+//Cyberchrome RAM repaired
+//Cyberchrome Physical memory repaired
+namespace cyber4();
+{
+	void cyberchrome();
+	void gpu();
+	void cpu();
+	void ram();
+	void physical_memory();
+	void repair();
+}
+
+//Cyberchrome virtual Hight Processor Object
+namespace csi();
+{
+	void cyberchrome();
+	void hpu();
+	void hight_processor_object();
+}
+
+//Cyberchrome repaired performance
+namespace cyber5();
+{
+	void cyberchrome();
+	void repair();
+	void performance();	
+}
+
+//Load and reload
+namespace loadreload();
+{
+	void reload();
+	void load();
+}
+
